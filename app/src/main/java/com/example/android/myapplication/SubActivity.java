2c04147b2da3123b66ff7248f535a0cdf18f40b2
Your Name <giyoon21c@gmail.com>
2017-03-31 10:32:28 -0700

    SubActivity with toolbar in progress

--- app/src/main/java/com/example/android/myapplication/SubActivity.java
+++ app/src/main/java/com/example/android/myapplication/SubActivity.java
@@ -3,22 +3,16 @@ package com.example.android.myapplication;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
-    private Toolbar toolbar;
+public class SubActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        toolbar = (Toolbar) findViewById(R.id.app_bar);
-        setSupportActionBar(toolbar);
+        setContentView(R.layout.activity_sub);
     }
 
     @Override
