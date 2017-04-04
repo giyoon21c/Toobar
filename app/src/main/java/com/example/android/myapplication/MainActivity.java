1b5aeb14aab5e925ed7f889d32882bd2b2ffc4e2
Your Name <giyoon21c@gmail.com>
2017-04-04 07:47:00 -0700

    listview with arrayadapter

--- app/src/main/java/com/example/android/myapplication/MainActivity.java
+++ app/src/main/java/com/example/android/myapplication/MainActivity.java
@@ -6,19 +6,46 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static android.R.layout.simple_list_item_1;
 
 public class MainActivity extends AppCompatActivity {
 
     private Toolbar toolbar;
+
+    ListView l;
+    String[] days = {"Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // toobar setup
         toolbar = (Toolbar) findViewById(R.id.app_bar);
         setSupportActionBar(toolbar);
+
+        // listadapter setup
+        l = (ListView) findViewById(R.id.listView);
+        // 2nd param in constructor is how the view should appear...
+        // Android's simple_list_item_1
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, days);
+        // set the adapter for this listView
+        l.setAdapter(adapter);
+        // define OnItemClickListener
+        l.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id){
+                TextView temp = (TextView) view;
+                Toast.makeText(getApplicationContext(), temp.getText() + "" + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
@@ -38,6 +65,8 @@ public class MainActivity extends AppCompatActivity {
             return true;
         }
 
+        // Intent is defined as calling SubActivity here when
+        // navigate submenu is chosen
         if (id == R.id.navigate) {
             startActivity(new Intent(this, SubActivity.class));
         }
